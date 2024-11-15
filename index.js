b36d908fb8afde4772be0a271639fae027d54865
ritushreedas2002 <ritushreedas20027@gmail.com>
2024-11-15 12:12:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+require("dotenv").config()
+const express = require("express");
+const connectDB = require("./config/db");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const cors = require("cors");
+const app = express();
+
+connectDB();
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5000"
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const swaggerOptions = {
+    definition: { 
+      openapi: "3.0.0",
+      info: {
+        title: "Car Management API",
+        version: "1.0.0",
+        description: "API for managing car inventory",
+      },
+      servers: [
+        {
+          url: "http://localhost:5000", 
+        },
+      ],
+    },
+    apis: ["./routes/*.js"], 
+  };
+
+  
+  const swaggerDocs = swaggerJsDoc(swaggerOptions);
+  const CSS_URL="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
+  
+  
+  app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs, {
+      customCss:
+          '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+      customCssUrl: CSS_URL
+  })
+  )
+
+
+
+app.get("/", ( res) => {
+  res.send("Welcome to the Conatct Management API");
+});
+
+// API Routes
+app.use("/contacts", require("./routes/contactRoutes"));
+
+
+
+app.listen(5000,()=>{
+    console.log('server running successfully');
+})
