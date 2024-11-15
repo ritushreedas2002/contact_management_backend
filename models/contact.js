a4047ea9e6b50e0ce0e32cba07f7279bd659fe68
ritushreedas2002 <ritushreedas20027@gmail.com>
2024-11-15 18:14:41 +0530

    changed the validation of our schema

--- models/contact.js
+++ models/contact.js
@@ -1,13 +1,33 @@
 const mongoose = require("mongoose");
+
 const contactSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName:  { type: String, required: true },
-    email:     { type: String, required: true, unique: true },
-    phoneNumber: { type: String, required: true },
-    company:   { type: String },
-    jobTitle:  { type: String },
-  });
-  
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid email!`
+    }
+  },
+  phoneNumber: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{10}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number! Phone number must be 10 digits.`
+    }
+  },
+  company: { type: String },
+  jobTitle: { type: String },
+});
+
 const Contact = mongoose.model('Contact', contactSchema);
 
-module.exports=Contact;
+module.exports = Contact;
