85fead448aaf991f5281f457b740b7b37e6cbd1c
ritushreedas2002 <ritushreedas20027@gmail.com>
2024-11-15 12:37:14 +0530

    changes in swagger

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: "http://localhost:5000",
+        url: "https://contact-management-backend-seven.vercel.app/",
       },
     ],
   },
